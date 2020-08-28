2cd433d8d20e4322809ef8ce821b231c14dbccaa
taosue <taosue314@gmail.com>
2020-08-28 11:48:42 +0800

    update title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Aragonchina</title>
+        <title>Aragon China</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href={logo}   rel="icon"/>
       </Head>
--- pages/onboarding.js
+++ pages/onboarding.js
@@ -8,7 +8,7 @@ const Onboarding = () => {
   return (
     <>
       <Head>
-        <title>Aragonchina</title>
+        <title>Aragon China</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="container">
